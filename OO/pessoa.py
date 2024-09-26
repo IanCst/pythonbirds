3e8365d509d27cc6938139042917683cb0b345d1
zerol <szerinhu@gmail.com>
2024-09-25 23:02:35 -0300

    Criado método cumprimentar

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = 23):
+        self.idade = idade
+        self.nome = nome
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Itália')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Ian'
+    print(p.nome)
+    print(p.idade)
