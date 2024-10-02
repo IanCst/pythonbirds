a29527f7f498d9b446942e883bda66629b9eee39
zerol <szerinhu@gmail.com>
2024-10-01 22:40:47 -0300

    metodos e acesso á classe

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,10 +1,20 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self,*filhos, nome = None, idade = 23):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá {id(self)}'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos{cls.olhos}'
 
 if __name__ == '__main__':
     Ian = Pessoa(nome='Ian')
@@ -16,3 +26,7 @@ if __name__ == '__main__':
     print(Italia.idade)
     for filho in Italia.filhos:
         print(filho.nome)
+    Italia.sobrenome = 'franca'
+    print(Italia.sobrenome)
+    print(Pessoa.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe())
